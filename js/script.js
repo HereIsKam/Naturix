b3fade61f327f23fc6c5de78abf370b401b06f81
HereIsKam <HereIsKam@users.noreply.github.com>
2020-06-04 11:15:18 +0300

    Corrects

--- js/script.js
+++ js/script.js
@@ -1,20 +1,18 @@
-var mySwiper = new Swiper('.hero-slider', {
-  direction: 'horizontal',
+var mySwiper = new Swiper(".hero-slider", {
+  direction: "horizontal",
   loop: true,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".hero-button-next",
+    prevEl: ".hero-button-prev",
   },
 });
 
-var swiper = new Swiper('.products-slider', {
-
-  direction: 'horizontal',
+var swiper = new Swiper(".products-slider", {
+  direction: "horizontal",
   loop: true,
   slidesPerView: 6,
   spaceBetween: 30,
   pagination: {
-    el: '.swiper-pagination',
+    el: ".products-pagination",
   },
-
 });
